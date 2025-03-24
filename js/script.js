b1f0a43eabab450ff1ed239a7e5270039d9d1af5
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-03-24 17:09:34 +0000

    All Jaavscript connection are success

--- js/script.js
+++ js/script.js
@@ -8,6 +8,16 @@
  * Displays an alert with the message "Hello, World!"
  */
 function myButtonClicked() {
-  document.getElementById('pay').innerHTML = `Your pay will be: $ ${takeHomeSalary.toFixed(2)}`
-  xx = parseFloat(document.getElementById('length-of-rectangle').value)
+  // input
+  const hourWorked = parseInt(document.getElementById('answer1').value)
+  const hourlyWage = parseInt(document.getElementById('answer2').value)
+
+  // process
+  let Incometax = (hours * hourlywage * ONTARIO_INCOME-TAX)
+  let takeHomeSalary = (hourlywage * 0.18)
+  
+  document.getElementById('answer1').innerHTML = `Your pay will be: $ ${takeHomeSalary.toFixed(2)}`
+  document.getElementById('answer2').innerHTML = 'The government will take...$' + takeHomeSalary.toFixed(2)
+  xx = parseFloat(document.getElementById('answer1').value)
+  xx = parseFloat(document.getElementById('answer2').value)
 }
